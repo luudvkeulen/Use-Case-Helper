ffd29b30676a9ba5c4ee8d21ac8a4afac73b8bf0
luudvkeulen <l.vankeulen@student.fontys.nl>
2015-09-09 13:32:44 +0200

    Nieuwe form g

---
+++ Use Case Helper V2/UseCaseInfo.Designer.cs
@@ -0,0 +1,243 @@
+﻿namespace Use_Case_Helper_V2
+{
+    partial class UseCaseInfo
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnBevestig = new System.Windows.Forms.Button();
+            this.tbResultaat = new System.Windows.Forms.TextBox();
+            this.tbUitzondering = new System.Windows.Forms.TextBox();
+            this.tbBeschrijving = new System.Windows.Forms.TextBox();
+            this.tbAannamen = new System.Windows.Forms.TextBox();
+            this.tbActors = new System.Windows.Forms.TextBox();
+            this.tbSamenv = new System.Windows.Forms.TextBox();
+            this.tbNaam = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnBevestig
+            // 
+            this.btnBevestig.Location = new System.Drawing.Point(8, 584);
+            this.btnBevestig.Name = "btnBevestig";
+            this.btnBevestig.Size = new System.Drawing.Size(147, 51);
+            this.btnBevestig.TabIndex = 29;
+            this.btnBevestig.Text = "Bevestig";
+            this.btnBevestig.UseVisualStyleBackColor = true;
+            this.btnBevestig.Click += new System.EventHandler(this.btnBevestig_Click);
+            // 
+            // tbResultaat
+            // 
+            this.tbResultaat.Location = new System.Drawing.Point(162, 519);
+            this.tbResultaat.Multiline = true;
+            this.tbResultaat.Name = "tbResultaat";
+            this.tbResultaat.Size = new System.Drawing.Size(561, 59);
+            this.tbResultaat.TabIndex = 28;
+            // 
+            // tbUitzondering
+            // 
+            this.tbUitzondering.Location = new System.Drawing.Point(162, 449);
+            this.tbUitzondering.Multiline = true;
+            this.tbUitzondering.Name = "tbUitzondering";
+            this.tbUitzondering.Size = new System.Drawing.Size(561, 59);
+            this.tbUitzondering.TabIndex = 27;
+            // 
+            // tbBeschrijving
+            // 
+            this.tbBeschrijving.Location = new System.Drawing.Point(162, 284);
+            this.tbBeschrijving.Multiline = true;
+            this.tbBeschrijving.Name = "tbBeschrijving";
+            this.tbBeschrijving.Size = new System.Drawing.Size(561, 159);
+            this.tbBeschrijving.TabIndex = 26;
+            // 
+            // tbAannamen
+            // 
+            this.tbAannamen.Location = new System.Drawing.Point(162, 216);
+            this.tbAannamen.Multiline = true;
+            this.tbAannamen.Name = "tbAannamen";
+            this.tbAannamen.Size = new System.Drawing.Size(561, 59);
+            this.tbAannamen.TabIndex = 25;
+            // 
+            // tbActors
+            // 
+            this.tbActors.Location = new System.Drawing.Point(162, 147);
+            this.tbActors.Multiline = true;
+            this.tbActors.Name = "tbActors";
+            this.tbActors.Size = new System.Drawing.Size(561, 59);
+            this.tbActors.TabIndex = 24;
+            // 
+            // tbSamenv
+            // 
+            this.tbSamenv.Location = new System.Drawing.Point(162, 78);
+            this.tbSamenv.Multiline = true;
+            this.tbSamenv.Name = "tbSamenv";
+            this.tbSamenv.Size = new System.Drawing.Size(561, 59);
+            this.tbSamenv.TabIndex = 23;
+            // 
+            // tbNaam
+            // 
+            this.tbNaam.Location = new System.Drawing.Point(162, 9);
+            this.tbNaam.Multiline = true;
+            this.tbNaam.Name = "tbNaam";
+            this.tbNaam.Size = new System.Drawing.Size(561, 59);
+            this.tbNaam.TabIndex = 22;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label7.Location = new System.Drawing.Point(8, 519);
+            this.label7.Name = "label7";
+            this.label7.Padding = new System.Windows.Forms.Padding(39, 20, 38, 20);
+            this.label7.Size = new System.Drawing.Size(147, 59);
+            this.label7.TabIndex = 21;
+            this.label7.Text = "Resultaat";
+            this.label7.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label6.Location = new System.Drawing.Point(8, 449);
+            this.label6.Name = "label6";
+            this.label6.Padding = new System.Windows.Forms.Padding(21, 20, 20, 20);
+            this.label6.Size = new System.Drawing.Size(147, 59);
+            this.label6.TabIndex = 20;
+            this.label6.Text = "Uitzonderingen";
+            this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label5.Location = new System.Drawing.Point(8, 284);
+            this.label5.Name = "label5";
+            this.label5.Padding = new System.Windows.Forms.Padding(31, 70, 30, 70);
+            this.label5.Size = new System.Drawing.Size(147, 159);
+            this.label5.TabIndex = 19;
+            this.label5.Text = "Beschrijving";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label4.Location = new System.Drawing.Point(8, 216);
+            this.label4.Name = "label4";
+            this.label4.Padding = new System.Windows.Forms.Padding(35, 20, 34, 20);
+            this.label4.Size = new System.Drawing.Size(147, 59);
+            this.label4.TabIndex = 18;
+            this.label4.Text = "Aannamen";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label3.Location = new System.Drawing.Point(8, 147);
+            this.label3.Name = "label3";
+            this.label3.Padding = new System.Windows.Forms.Padding(44, 20, 44, 20);
+            this.label3.Size = new System.Drawing.Size(147, 59);
+            this.label3.TabIndex = 17;
+            this.label3.Text = "Actoren";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label2.Location = new System.Drawing.Point(8, 78);
+            this.label2.Name = "label2";
+            this.label2.Padding = new System.Windows.Forms.Padding(26, 20, 25, 20);
+            this.label2.Size = new System.Drawing.Size(147, 59);
+            this.label2.TabIndex = 16;
+            this.label2.Text = "Samenvatting";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.Location = new System.Drawing.Point(8, 9);
+            this.label1.Name = "label1";
+            this.label1.Padding = new System.Windows.Forms.Padding(50, 20, 50, 20);
+            this.label1.Size = new System.Drawing.Size(147, 59);
+            this.label1.TabIndex = 15;
+            this.label1.Text = "Naam";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // UseCaseInfo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(733, 643);
+            this.Controls.Add(this.btnBevestig);
+            this.Controls.Add(this.tbResultaat);
+            this.Controls.Add(this.tbUitzondering);
+            this.Controls.Add(this.tbBeschrijving);
+            this.Controls.Add(this.tbAannamen);
+            this.Controls.Add(this.tbActors);
+            this.Controls.Add(this.tbSamenv);
+            this.Controls.Add(this.tbNaam);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "UseCaseInfo";
+            this.Text = "UseCase";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnBevestig;
+        private System.Windows.Forms.TextBox tbResultaat;
+        private System.Windows.Forms.TextBox tbUitzondering;
+        private System.Windows.Forms.TextBox tbBeschrijving;
+        private System.Windows.Forms.TextBox tbAannamen;
+        private System.Windows.Forms.TextBox tbActors;
+        private System.Windows.Forms.TextBox tbSamenv;
+        private System.Windows.Forms.TextBox tbNaam;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ Use Case Helper V2/UseCaseInfo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Use_Case_Helper_V2
+{
+    public partial class UseCaseInfo : Form
+    {
+        public UseCaseInfo()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBevestig_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
